558012e357f20f380a07d1d949729c67c3fba4e7
Dani Matuko <danimatuko@pop-os.lan>
2024-12-04 11:35:48 +0200

    add calls  Controller

---
+++ app/Http/Controllers/CallReportController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CallReportController extends Controller
+{
+    public function index(Request $request)
+    {
+        $calls = \App\Models\Call::with(['customer', 'agent'])
+            ->when($request->input('agent_id'), function ($query, $agentId) {
+                $query->where('agent_id', $agentId);
+            })
+            ->when($request->input('start_date') && $request->input('end_date'), function ($query) use ($request) {
+                $query->whereBetween('call_time', [$request->start_date, $request->end_date]);
+            })
+            ->paginate(10);
+
+        $agents = \App\Models\Agent::all();
+
+        return view('calls.index', compact('calls', 'agents'));
+    }
+
+}
